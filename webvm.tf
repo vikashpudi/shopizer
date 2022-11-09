584c7efb4309e6fedd0d3e15e6750154f1ba2b2b
murrali krishna <pudivikash@gmail.com>
2022-11-09 02:50:39 -0800

    my exepc

--- webvm.tf
+++ webvm.tf
@@ -1,10 +1,10 @@
 resource "azurerm_linux_virtual_machine" "webvm" {
   resource_group_name = var.resourcegroup_details.name
   location            = var.resourcegroup_details.location
-  name = format("webvm-%s",terraform.workspace)
-  size = "Standard_B1s"
-  admin_username = "qtdevops"
-  admin_password = "qttesting@1234"
+  name                = format("webvm-%s", terraform.workspace)
+  size                = "Standard_B1s"
+  admin_username      = "qtdevops"
+  admin_password      = "qttesting@1234"
   network_interface_ids = [
     azurerm_network_interface.web_nic.id
   ]
@@ -24,31 +24,40 @@ resource "azurerm_linux_virtual_machine" "webvm" {
 }
 
 resource "null_resource" "cluster" {
-  # Changes to any instance of the cluster requires re-provisioning
+  # 
   triggers = {
     version = var.runningversion
   }
 
-  # Bootstrap script can run on any instance of the cluster
-  # So we just choose the first in this case
-  
+
+
 
   provisioner "remote-exec" {
-    # Bootstrap script called with private_ip of each node in the cluster
+
     inline = [
       "sudo apt update",
       "sudo apt install nginx -y"
     ]
 
     connection {
-     host = azurerm_linux_virtual_machine.webvm.public_ip_address
-     user = "qtdevops"
-     password = "qttesting@1234"
-     
+      host     = azurerm_linux_virtual_machine.webvm.public_ip_address
+      user     = "qtdevops"
+      password = "qttesting@1234"
+
     }
+  }
+
+  provisioner "local-exec" {
+    command = "sudo ansible-playbook -i ${azurerm_network_interface.web_nic.publicip} playbook.yaml"
   }
 
   depends_on = [
     azurerm_linux_virtual_machine.webvm
   ]
+
+
+
 }
+
+
+
